a7e46859d99eea7693788821e57207c7c7dea7a5
HuyAn <huyanh170205@gmail.com>
2024-12-18 00:44:11 +0100

    Meine Änderungen

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-function mybutton(){
+function b1(){
     var x = document.getElementById("text1").value;
     document.getElementById("text2").value = x;
+}
+function b2(){
+    var x = document.getElementById("text2").value;
+    document.getElementById("p1").innerText = x;
 }

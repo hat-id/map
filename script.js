52e2130a9509a62fdedf9665dd91184eebfe02b2
HuyAn <huyanh170205@gmail.com>
2024-12-27 00:18:38 +0100

    Meine Änderungen

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+var zahl = 0;
 function b1(){
     var x = document.getElementById("text1").value;
     document.getElementById("text2").value = x;
+    zahl += 1;
+    document.getElementById("p2").innerText = zahl;
 }
+
 function b2(){
     var x = document.getElementById("text2").value;
     document.getElementById("p1").innerText = x;
+    zahl += 1;
+    document.getElementById("p2").innerText = zahl;
 }

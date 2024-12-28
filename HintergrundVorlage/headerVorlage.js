b80334a6bc106980481e1ddd0f4dbadd9d433803
HuyAn <huyanh170205@gmail.com>
2024-12-28 22:35:40 +0100

    Meine Änderungen

---
+++ HintergrundVorlage/headerVorlage.js
@@ -0,0 +1,14 @@
+class SpecialHeader extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <p style="display: flex;"> 
+            <div class="Leiste"></div>
+            <h1 class="Überschrift"> hat.id </h1>
+            <a class="home" href="/index.html">Home</a>
+            <a class="page1" href="/Todo-Liste.html">Todo-Liste</a>
+            <a class="Spiel1" href="/Spiel/Spiel1.html">Spiedwdwl</a>
+        </p>
+        `
+    }
+}
+customElements.define('special-header', SpecialHeader);
--- script.js
+++ script.js
@@ -5,10 +5,12 @@ function b1(){
     zahl += 1;
     document.getElementById("p2").innerText = zahl;
 }
-
 function b2(){
     var x = document.getElementById("text2").value;
     document.getElementById("p1").innerText = x;
     zahl += 1;
     document.getElementById("p2").innerText = zahl;
+    z = true;
 }
+
+

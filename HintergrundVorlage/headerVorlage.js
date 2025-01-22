08a4547b7c7647c18c433af86ed84abca2c80b32
HuyAn <huyanh170205@gmail.com>
2025-01-22 23:17:21 +0100

    Meine Änderungen

--- HintergrundVorlage/headerVorlage.js
+++ HintergrundVorlage/headerVorlage.js
@@ -1,17 +1,14 @@
 class SpecialHeader extends HTMLElement {
-    constructor() {
-        super();
-    }
     connectedCallback() {
         this.innerHTML = `
         <p style="display: flex;"> 
+        <link rel = "stylesheet" href="/HintergrundVorlage/headerCss.css">
             <div class="Leiste"></div>
             <h1 class="Überschrift"> hat.id </h1>
-            <a class="home" href="/index.html">Home</a>
-            <a class="page1" href="/Todo-Liste.html">Todo-Liste</a>
-            <a class="Spiel1" href="/Spiel/Spiel1.html">Spiedwdwl</a>
+            <a class="home" href="/MAP/index.html">Home</a>
+            <a class="page1" href="/MAP/Todo-Liste.html">Todo-Liste</a>
+            <a class="Spiel1" href="/MAP/Spiel/Spiel1.html">Spiedwdwl</a>
         </p>
         `
     }
 }
-customElements.define('special-header', SpecialHeader);

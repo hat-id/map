c2b5bd35a8f4016e33f0b4cc35edc00cbdb5f9b7
HuyAn <huyanh170205@gmail.com>
2025-01-22 22:06:07 +0100

    Meine Änderungen

--- HintergrundVorlage/headerVorlage.js
+++ HintergrundVorlage/headerVorlage.js
@@ -2,7 +2,6 @@ class SpecialHeader extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
         <p style="display: flex;"> 
-        <link rel = "stylesheet" href="/HintergrundVorlage/headerCss.css">
             <div class="Leiste"></div>
             <h1 class="Überschrift"> hat.id </h1>
             <a class="home" href="/MAP/index.html">Home</a>
